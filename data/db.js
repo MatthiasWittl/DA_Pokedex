09b3dbe6abd8c734efa6891ffa494d4d5798f120
MatthiasWittl <Matthias_Wittl@outlook.de>
2026-01-06 11:44:03 +0100

    background colors for pokemon types added in db and for render

--- data/db.js
+++ data/db.js
@@ -4,5 +4,25 @@ let pokemonNames1;
 
 let pokemonData = {};
 
-let firstAmountOfLoadingPokemon = 151; // max Amount Pokemon Lets Go Pickachu
+let firstAmountOfLoadingPokemon = 151; 
+
+const typeColors = {
+    bug: "#A8B820",
+    dragon: "#7038F8",
+    electric: "#F8D030",
+    fairy: "#EE99AC",
+    fighting: "#C03028",
+    fire: "#F08030",
+    flying: "#A890F0",
+    ghost: "#705898",
+    grass: "#78C850",
+    ground: "#E0C068",
+    ice: "#98D8D8",
+    normal: "#A8A878",
+    poison: "#A040A0",
+    psychic: "#F85888",
+    rock: "#B8A038",
+    steel: "#B8B8D0",
+    water: "#6890F0"
+  };
 
--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,13 @@
 function renderPokemonBox(index) {
     if (pokemonData[index].value.types.length == 1) {
+        let pokemonType = pokemonData[index].value.types[0].type.name
+        let pokemonTypeColor = typeColors[pokemonType];
     return `
      <section class="pokemon_box">
             <h1>${pokemonData[index].value.name}</h1>
-            <div class="pokemon_box_img_container">
-                <img class="pokemon_box_img" src="${pokemonData[index].value.sprites.other['official-artwork'].front_default}" alt="">
+            <div class="pokemon_box_img_container pkmn_box_color" style= "--type1: ${pokemonTypeColor};">
+                <img class="pokemon_box_img" 
+                src="${pokemonData[index].value.sprites.other['official-artwork'].front_default}" alt="">
             </div>
             <div>
                 <p>${pokemonData[index].value.height}</p>
@@ -13,11 +16,18 @@ function renderPokemonBox(index) {
 
         </section>
     ` } else {
+        let pokemonTypeOne = pokemonData[index].value.types[0].type.name
+        let pokemonTypeColor = typeColors[pokemonTypeOne];
+        let pokemonTypeTwo = pokemonData[index].value.types[1].type.name
+        let pokemonTypeColorTwo = typeColors[pokemonTypeTwo];
         return `
      <section class="pokemon_box">
             <h1>${pokemonData[index].value.name}</h1>
-            <div class="pokemon_box_img_container">
-                <img class="pokemon_box_img" src="${pokemonData[index].value.sprites.other['official-artwork'].front_default}" alt="">
+            <div class="pokemon_box_img_container pkmn_box_color" 
+                    style= "--type1: ${pokemonTypeColor}; --type2: ${pokemonTypeColorTwo};">
+                <img 
+                    class="pokemon_box_img" 
+                    src="${pokemonData[index].value.sprites.other['official-artwork'].front_default}" alt="">
             </div>
             <div>
                 <p>${pokemonData[index].value.height}</p>
