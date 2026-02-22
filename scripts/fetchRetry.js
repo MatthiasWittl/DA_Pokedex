e25a0a7042f06e3bdbf972d53ff67c2d4fad809f
MatthiasWittl <Matthias_Wittl@outlook.de>
2026-02-22 20:03:35 +0100

    retry fetch notes deleted and prettier

--- scripts/fetchRetry.js
+++ scripts/fetchRetry.js
@@ -1,18 +1,16 @@
 async function retryFetch(url, retries = 3) {
-    try {
-        const response = await fetch(url);
+  try {
+    const response = await fetch(url);
 
-        if (!response.ok) {
-            //console.error(`${response.status} - Retry left:  ${retries}`);
-            if (response.status >= 500 && retries > 0) {
-                return retryFetch(url, retries - 1);
-            }
-            //throw new Error(`Request failed width ${response.status}`);
-        }
-        return await response.json();
-    } catch (error) {
-      if (retries > 0 && error instanceof TypeError) {
+    if (!response.ok) {
+      if (response.status >= 500 && retries > 0) {
         return retryFetch(url, retries - 1);
+      }
     }
-}}
-
+    return await response.json();
+  } catch (error) {
+    if (retries > 0 && error instanceof TypeError) {
+      return retryFetch(url, retries - 1);
+    }
+  }
+}
