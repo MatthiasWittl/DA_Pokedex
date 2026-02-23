07ac53f216a784a7bda048d6c805e8b38947257a
MatthiasWittl <Matthias_Wittl@outlook.de>
2026-02-23 08:52:53 +0100

    template js html part viewed and restyled

--- scripts/template.js
+++ scripts/template.js
@@ -33,24 +33,24 @@ function renderSingleViewPokemonBox(index, colorOne, colorTwo) {
   return `
     <button aria-label="Button to Open Dialog Box for Details" onclick="closeDialog()" style="--type1: ${colorOne}" class="closing_Button_dialog_Box">X</button>
     <section class="single_view_Pokemon_Box" style="--type1: ${colorOne}; --type2: ${colorTwo};">
-        <h2 class="pokemon_box_title_glow">${pokemonData[index].name} #${pokemonData[index].id}</h2>
-        <div class="pokemon_box_img_container pkmn_box_color">
-            <img class="pokemon_box_img" src="${pokemonData[index].sprites.other["official-artwork"].front_default}"
-            alt="">
-        </div>
-        <section class="pokemon_single_view_data_header">
-        <button id="battle_stats_btn" onclick="showBattleStats(${index})" >Battle Stats</button>
-        <button id="pkmn_moves_btn" onclick="showPkmnMoves(${index})" >Moves</button>
-        <button id="evo_chain_btn" onclick="evoChainImgSet(${index})" >Evo Chain</button>
-        </section>
-            <section id="swap_container_id" class="swap_container">
-            <dl id="battle_stats_datalist">
-            </dl>
-            </section>
-            <section class="single_view_switch_container">
-            <button aria-label="Button to previous Pokemon" class="single_view_switch" onclick="singleViewSwitch('previous', ${index})" >&#10094</button>
-            <button aria-label="Button to next Pokemon" class="single_view_switch" onclick="singleViewSwitch('next', ${index})">&#10095</button>
-            </section>
+      <h2 class="pokemon_box_title_glow">${pokemonData[index].name} #${pokemonData[index].id}</h2>
+      <div class="pokemon_box_img_container pkmn_box_color">
+        <img class="pokemon_box_img" src="${pokemonData[index].sprites.other["official-artwork"].front_default}"
+          alt="">
+      </div>
+    <section class="pokemon_single_view_data_header">
+      <button id="battle_stats_btn" onclick="showBattleStats(${index})" >Battle Stats</button>
+      <button id="pkmn_moves_btn" onclick="showPkmnMoves(${index})" >Moves</button>
+      <button id="evo_chain_btn" onclick="evoChainImgSet(${index})" >Evo Chain</button>
+    </section>
+    <section id="swap_container_id" class="swap_container">
+      <dl id="battle_stats_datalist">
+      </dl>
+    </section>
+    <section class="single_view_switch_container">
+      <button aria-label="Button to previous Pokemon" class="single_view_switch" onclick="singleViewSwitch('previous', ${index})" >&#10094</button>
+      <button aria-label="Button to next Pokemon" class="single_view_switch" onclick="singleViewSwitch('next', ${index})">&#10095</button>
+    </section>
             
     
 `;
